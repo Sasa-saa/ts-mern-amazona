f6cddde991088aa0a6b7775ce7ce1e504721a7da
Saa-saa <k.o_sisami@hotmail.com>
2025-08-19 15:59:34 +0100

    add reactBootstrap, edits to app.css, app.tsx, main.tsx and index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,44 @@
-// import { useState } from "react"
-// import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap"; // Importing Bootstrap components
 import { sampleProducts } from "./data"; // Importing sample products
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <header>TS Amazona</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">tsAmazona</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>All rights reserved. Amazona 2025</footer>
+      <footer>
+        <div className="text-center">All rights reserved. Amazona 2025</div>
+      </footer>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importing Bootstrap CSS from node module folder
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
