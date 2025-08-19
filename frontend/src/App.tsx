f000973fd8f3494b5f9f41d1c801ca0f6e75c1fc
Saa-saa <k.o_sisami@hotmail.com>
2025-08-19 14:40:19 +0100

    List-Protucts files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-// import { useState } from "react";
+// import { useState } from "react"
 // import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
+import { sampleProducts } from "./data"; // Importing sample products
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -9,7 +9,21 @@ function App() {
   return (
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights reserved. Amazona 2025</footer>
     </div>
   );
